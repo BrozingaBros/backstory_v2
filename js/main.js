00a38e97a306201bfddd124d44b91388f04c207a
Sebastian Vuye <sebastian.vuye@gmail.com>
2018-11-23 21:23:05 +0100

    add slideshow index

--- js/main.js
+++ js/main.js
@@ -3,3 +3,14 @@ menuButton.addEventListener("click", function(){
    var menu = document.getElementById("menu");
    menu.classList.toggle("nav-body-list-wrapper--shown");
 });
+
+$("#slideshow > figure:gt(0)").hide();
+
+setInterval(function() {
+    $('#slideshow > figure:first')
+        .fadeOut(1000)
+        .next()
+        .fadeIn(1000)
+        .end()
+        .appendTo('#slideshow');
+},  4000);
