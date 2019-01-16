eb4d997738b996f98109af00a4d9b371c4850ce3
Zishan Khan <zishan@pau.be>
2019-01-16 21:10:41 +0100

    fix from mail

--- form.php
+++ form.php
@@ -1,11 +1,12 @@
 <?php
 $formData = $_POST;
-$message = "Beste, U heeft een bericht ontvangen op backstory.be van " . $formData['clientName'] . "als bericht: \n " . $formData['clientComment'] . " \n
+$message = "Beste, U heeft een bericht ontvangen op backstory.be van " . $formData['clientName'] . " als bericht: \n " . $formData['clientComment'] . " \n
 De overige gegevens zijn: \n
 Partner naam:" . $formData['partnerName'] . "\n
 Client e-mailadres:" . $formData['clientEmail'] . "\n
 ";
-$mail = mail('sebastian.vuye@gmail.com', 'Contactformulier Backstory', $message);
+$headers = "From: hello@backstory.be" . "\r\n";
+$mail = mail('sebastian.vuye@gmail.com', 'Contactformulier Backstory', $message, $headers);
 if ($mail) {
     include "contact_s.html";
 }
