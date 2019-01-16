49c81592a4471240bf17a73ff222328953b64dd8
Zishan Khan <zishan@pau.be>
2019-01-16 21:11:18 +0100

    add spacing

--- form.php
+++ form.php
@@ -2,8 +2,8 @@
 $formData = $_POST;
 $message = "Beste, U heeft een bericht ontvangen op backstory.be van " . $formData['clientName'] . " als bericht: \n " . $formData['clientComment'] . " \n
 De overige gegevens zijn: \n
-Partner naam:" . $formData['partnerName'] . "\n
-Client e-mailadres:" . $formData['clientEmail'] . "\n
+Partner naam: " . $formData['partnerName'] . "\n
+Client e-mailadres: " . $formData['clientEmail'] . "\n
 ";
 $headers = "From: hello@backstory.be" . "\r\n";
 $mail = mail('sebastian.vuye@gmail.com', 'Contactformulier Backstory', $message, $headers);
